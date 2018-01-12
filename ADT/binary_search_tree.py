07f2558e32a045b0452e9d5b8db893bb6dafa485
zqun <athemeroy@126.com>
2018-01-12 16:10:57 +0800

    add linked_list.

--- ADT/binary_search_tree.py
+++ ADT/binary_search_tree.py
@@ -1,21 +1,15 @@
-
-            
-class BinarySearchTree():
+class BinarySearchTree:
     def __init__(self, init=None):
         self.root = None
         if init:
             self.insert(init)
 
-        
-        
-        
     class Node():
         def __init__(self, key):
             self.key = key
             self.right = None
             self.left = None
-            
-            
+
         def __repr__(self):
             return f'<Node key: {self.key} left: {self.left} right: {self.right} \n>'
 
@@ -29,11 +23,11 @@ class BinarySearchTree():
             left_depth = self._max_depth_node(node.left)
             right_depth = self._max_depth_node(node.right)
             return max(left_depth, right_depth) + 1
-        
+
     @property
     def max_depth(self):
         return self._max_depth_node(self.root)
-    
+
     def _min_depth_node(self, node):
         if node == None:
             return 0
@@ -51,13 +45,11 @@ class BinarySearchTree():
             left_min_depth = self._min_depth_node(node.left)
             right_min_depth = self._min_depth_node(node.right)
             return min(left_min_depth, right_min_depth) + 1
-            
-            
+
     @property
     def min_depth(self):
         return self._min_depth_node(self.root)
-        
-        
+
     def _insert_node(self, node, new_node):
         # 总是向最后一行加入新的节点
         if node.key > new_node.key:
@@ -70,7 +62,7 @@ class BinarySearchTree():
                 node.right = new_node
             else:
                 self._insert_node(node.right, new_node)
-                
+
     def insert(self, key):
         if hasattr(key, '__iter__'):
             for i in key:
@@ -81,69 +73,63 @@ class BinarySearchTree():
                 self.root = new_node
             else:
                 self._insert_node(self.root, new_node)
-            
 
     def __repr__(self):
         return self.root.__repr__()
-    
+
     def _in_order_traverse_node(self, node, callback):
         if node != None:
             self._in_order_traverse_node(node.left, callback)
             callback(node.key)
             self._in_order_traverse_node(node.right, callback)
-            
-    
+
     def in_order_traverse(self, callback):
         return self._in_order_traverse_node(self.root, callback)
-    
+
     def show(self):
         return self.in_order_traverse(print)
-
 
     def _pre_order_traverse_node(self, node, callback):
         if node != None:
             callback(node.key)
             self._pre_order_traverse_node(node.left, callback)
             self._pre_order_traverse_node(node.right, callback)
-            
-    
+
     def pre_order_traverse(self, callback):
         return self._pre_order_traverse_node(self.root, callback)
-
 
     def _post_order_traverse_node(self, node, callback):
         if node != None:
             self._post_order_traverse_node(node.left, callback)
             self._post_order_traverse_node(node.right, callback)
             callback(node.key)
-    
+
     def post_order_traverse(self, callback):
         return self._post_order_traverse_node(self.root, callback)
-    
+
     def _min_node(self, node):
         if node:
             while (node and node.left != None):
                 node = node.left
             return node
-    
-    
-#    def _find_min_node(self):
-        
-        
+
+
+        #    def _find_min_node(self):
+
     @property
     def min(self):
         return self._min_node(self.root).key
-    
+
     def _max_node(self, node):
         if node:
             while (node and node.right != None):
                 node = node.right
             return node
-        
+
     @property
     def max(self):
         return self._max_node(self.root).key
-    
+
     def _search_node(self, node, key):
         if node == None:
             return False
@@ -153,10 +139,10 @@ class BinarySearchTree():
             return self._search_node(node.right, key)
         else:
             return True
-    
+
     def search(self, key):
         return self._search_node(self.root, key)
-    
+
     def _remove_node(self, node, key):
         """
         父节点总是会接收到函数的返回值，用来处理指针问题。
@@ -186,22 +172,19 @@ class BinarySearchTree():
             else:
                 # 找到这个节点的最小的后代 这个后代被称为继承者
                 aux = self._min_node(node.right)
-#                print(f'aux is now{aux}\n\n')
+                #                print(f'aux is now{aux}\n\n')
                 # 用这个后代的key代替这个节点的key 可以说这个节点已经被移除了
                 node.key = aux.key
                 # 但是这个时候出现两个相同键的节点
                 # 在它的后代（只有可能在右支）中移除这个节点
                 node.right = self._remove_node(node.right, aux.key)
-#                print(f'{node}.right is {node.right}')
+                #                print(f'{node}.right is {node.right}')
                 return node
-            
-    
+
     def remove(self, key):
         self.root = self._remove_node(self.root, key)
-    
-    
+
+
 if __name__ == '__main__':
     bst = BinarySearchTree()
     bst.insert([11, 7, 15, 5, 9, 13, 20, 3, 6, 8, 10, 12, 14, 18, 25])
-
-                    
---
+++ ADT/linked_list.py
@@ -0,0 +1,23 @@
+# todo: ll
+class Node:
+    def __init__(self, pre=None, data=None):
+        self.data = data
+        self.pred = pre
+        self.succ = None
+
+    def insert_pred(self, node):
+        pass
+
+    def insert_succ(self, node):
+        pass
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def search(self, value):
+        pass
+
+    def get(self, postion):
+        pass
+
